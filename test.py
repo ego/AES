f31f7bc3e78b9f68cf184341635b19240be6bfa7
Vice-Versa <0x7c48@gmail.com>
2023-01-10 03:17:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from aes_ecb_pkcs5padding_128_256 import encrypt, decrypt
 
-text1 = "Алена Лазуткина"
-text2 = "Vlad Ischenko"
+text1 = "John Doe"
+text2 = "Martin Fowler"
 
 decrypt(encrypt(text1)) == text1
 decrypt(encrypt(text2)) == text2
